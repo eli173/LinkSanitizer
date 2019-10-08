006db743f912f420d7276b7fe112f55d71849f3b
Elijah Cohen <eli@eli173.com>
2019-10-08 15:01:19 -0500

    now I've got nice link passthrough, I can think about starting to focus on link modification now!

--- app/src/main/java/com/eli173/linksanitizer/MainActivity.kt
+++ app/src/main/java/com/eli173/linksanitizer/MainActivity.kt
@@ -1,19 +1,63 @@
 package com.eli173.linksanitizer
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val sampleUri = Uri.parse("https://eli173.com")
+        val browserFinderIntent = Intent(Intent.ACTION_VIEW, sampleUri)
+
+        var browsers = packageManager.queryIntentActivities(browserFinderIntent, PackageManager.MATCH_ALL)
+        Log.d("173", browsers.map{ x -> x.activityInfo.packageName}.toString())
+        browsers = browsers.filter { b -> b.activityInfo.packageName != "com.eli173.linksanitizer" }
+
         setContentView(R.layout.activity_main)
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_key), Context.MODE_PRIVATE)
+        prefs.contains("browser")
+
+        val rg = findViewById<RadioGroup>(R.id.radiogrp)
+        for (b in browsers) {
+            val button = RadioButton(this)
+            if(b.activityInfo.packageName == prefs.getString("browser", "")) {
+                button.toggle()
+            }
+            button.text = b.activityInfo.packageName
+            button.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked) {
+                    val x = b.activityInfo.packageName
+                    Log.d("173", "found the button listener for $x")
+                    with(prefs.edit()) {
+                        putString("browser", x)
+                        commit()
+                    }
+                }
+            }
+            rg.addView(button)
+        }
+
     }
 
     fun promptDefault(view: View) {
+        val defIntent = Intent(android.provider.Settings.ACTION_SETTINGS)
         Log.d("173","prompt default reached")
-
+        startActivityForResult(defIntent, 0)
+        /*
+        Log.d("173",defIntent.resolveActivity(packageManager).toString())
+        if(defIntent.resolveActivity(packageManager) != null) {
+            startActivity(defIntent)
+        }*/
     }
 }
--- app/src/main/java/com/eli173/linksanitizer/SanitizeActivity.kt
+++ app/src/main/java/com/eli173/linksanitizer/SanitizeActivity.kt
@@ -1,5 +1,8 @@
 package com.eli173.linksanitizer
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -9,6 +12,36 @@ class SanitizeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         Log.d("173", intent.dataString)
+
+        val uri = intent.data
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_key), Context.MODE_PRIVATE)
+
+        val defaultBrowserPkg = prefs.getString("browser","")
+
+        // TODO: check for installs, do something when default isn't selected
+
+        if (defaultBrowserPkg != "") {
+            // it's well-defined and should work, if it's still installed
+            val browseIntent = Intent(Intent.ACTION_VIEW, uri)
+            browseIntent.setPackage(defaultBrowserPkg)
+            if(browseIntent.resolveActivity(packageManager) != null) {
+                startActivity(browseIntent)
+            }
+        }
+
+        /*
+        val browseIntent = Intent(Intent.ACTION_VIEW, intent.data)
+        val title = resources.getString(R.string.choose_browser)
+        val chooser = Intent.createChooser(browseIntent, title)
+        val acts = packageManager.queryIntentActivities(browseIntent,PackageManager.MATCH_DEFAULT_ONLY)
+        Log.d("173",acts.toString())
+        Log.d("173",browseIntent.resolveActivity(packageManager).toString())
+        //if (browseIntent.resolveActivity(packageManager) != null) {
+        startActivity(chooser)
+        //}
+        */
+
     }
 
 }
