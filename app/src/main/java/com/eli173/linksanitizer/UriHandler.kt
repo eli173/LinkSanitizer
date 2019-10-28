8b1e22f965f503be4dc0e581d407dab1fc909a99
Elijah Cohen <eli@eli173.com>
2019-10-28 12:25:38 -0500

    properly doing ui thread stuff now I think?

--- app/src/main/java/com/eli173/linksanitizer/UriHandler.kt
+++ app/src/main/java/com/eli173/linksanitizer/UriHandler.kt
@@ -2,19 +2,26 @@ package com.eli173.linksanitizer
 
 import android.net.Uri
 import android.os.AsyncTask
-import android.util.Log
 import android.widget.TextView
+import java.lang.ref.WeakReference
 
-abstract class UriHandler(val nextHandler: UriHandler?, val textView: TextView): AsyncTask<Uri, Unit, Uri>() {
+abstract class UriHandler(private val nextHandler: UriHandler?, textView: TextView): AsyncTask<Uri, Unit, Uri>() {
     abstract val classString: String
     abstract fun backgroundTask(uri: Uri): Uri
 
+    private val tv = WeakReference<TextView>(textView)
+
+    final override fun onPreExecute() {
+        super.onPreExecute()
+
+    }
+
     final override fun doInBackground(vararg params: Uri): Uri {
-        return backgroundTask(params.get(0))
+        return backgroundTask(params[0])
     }
 
     override fun onPostExecute(result: Uri) {
-        textView.append("\n${classString}: ${result.toString()}")
+        tv.get()?.append("\n${classString}: ${result}")
         nextHandler?.execute(result)
     }
 
